2828a273aa0a23d9f0d319d0d3ace6ce77d15560
freshLiver <freshliver.cys@gmail.com>
2020-12-17 01:07:52 +0800

    make gallery_id string ('#xxxxxx')

--- nhlib/nhBook.py
+++ nhlib/nhBook.py
@@ -5,25 +5,30 @@ from nhlib.nhRequest import *
 class NhBook :
     
     def __init__ ( self ) :
-        self.__title = ""
+        # @formatter:off
+        self.__title        = ""
         self.__gallery_link = ""
-        self.__gallery_id = 0
-        self.__total_pages = 0
-        self.__favorites = 0
-        self.__images = list( )
+        self.__gallery_id   = ""
+        self.__total_pages  = 0
+        self.__favorites    = 0
+        self.__images       = list( )
+        # @formatter:on
     
     
-    def set_new_book ( self, link: str ) :
+    def check_this_book ( self, link: str ) :
         # use url to get gallery info
-        target = NhRequest.get_book_from_gallery( link )
+        details: [str, str, str, int, int, list]
+        details = NhRequest.get_detail_from_gallery( link )
         
+        # @formatter:off
         # get all properties of target gallery
-        self.__title = target.__title
-        self.__gallery_link = target.__gallery_link
-        self.__gallery_id = target.__gallery_id
-        self.__total_pages = target.__total_pages
-        self.__favorites = target.__favorites
-        self.__images = target.__images
+        self.__title        = details[0]
+        self.__gallery_link = details[1]
+        self.__gallery_id   = details[2]
+        self.__total_pages  = details[3]
+        self.__favorites    = details[4]
+        self.__images       = details[5]
+        # @formatter:on
     
     
     def get_total_pages ( self ) -> int :
@@ -34,16 +39,16 @@ class NhBook :
         return self.__images[page]
     
     
-    def reset ( self ) :
-        self.__title = ""
+    def close_this_book ( self ) :
+        # @formatter:off
+        self.__title        = ""
         self.__gallery_link = ""
-        self.__gallery_id = 0
-        self.__total_pages = 0
-        self.__favorites = 0
+        self.__gallery_id   = ""
+        self.__total_pages  = 0
+        self.__favorites    = 0
+        # @formatter:on
         self.__images.clear( )
-    
     
     # **********************************************************
     # ********************* private methods ********************
     # **********************************************************
-    
