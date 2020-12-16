83890129841514407fd94ce2a407d8f91c570e0d
freshLiver <freshliver.cys@gmail.com>
2020-12-16 14:18:10 +0800

    rn pages -> images & add total_pages & imple set_new_book

--- nhlib/nhBook.py
+++ nhlib/nhBook.py
@@ -1,14 +1,29 @@
+from nhlib.nhGallery import *
+from nhlib.nhRequest import *
+
+
 class NhBook :
     
     def __init__ ( self ) :
         self.__title = ""
+        self.__gallery_link = ""
+        self.__gallery_id = 0
         self.__total_pages = 0
-        self.__pages = list( )
+        self.__favorites = 0
+        self.__images = list( )
     
     
-    def set_new_book ( self, gallery ) :
-        # TODO
-        pass
+    def set_new_book ( self, link: str ) :
+        # use url to get gallery info
+        target = NhRequest.get_book_from_gallery( link )
+        
+        # get all properties of target gallery
+        self.__title = target.__title
+        self.__gallery_link = target.__gallery_link
+        self.__gallery_id = target.__gallery_id
+        self.__total_pages = target.__total_pages
+        self.__favorites = target.__favorites
+        self.__images = target.__images
     
     
     def get_total_pages ( self ) -> int :
@@ -16,10 +31,19 @@ class NhBook :
     
     
     def get_link_of_page ( self, page: int ) -> str :
-        return self.__pages[page]
+        return self.__images[page]
     
     
     def reset ( self ) :
         self.__title = ""
+        self.__gallery_link = ""
+        self.__gallery_id = 0
         self.__total_pages = 0
-        self.__pages = list( )
+        self.__favorites = 0
+        self.__images.clear( )
+    
+    
+    # **********************************************************
+    # ********************* private methods ********************
+    # **********************************************************
+    
