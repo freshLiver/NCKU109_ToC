fa46290e84de1cc9e17115bf4dd1c7ea405ad251
freshLiver <freshliver.cys@gmail.com>
2020-12-18 22:19:52 +0800

    move show current state into NhReply

--- nhlib/nhReply.py
+++ nhlib/nhReply.py
@@ -1,3 +1,6 @@
+from nhlib.nhEyes import *
+
+
 class NhReply :
     
     def __init__ ( self ) :
@@ -22,9 +25,13 @@ class NhReply :
         self.__messages.clear( )
     
     
-    def get_reply_message ( self ) -> [str or None, str] :
-        reply = ""
+    def get_reply_message ( self, current_state: NhCommand ) -> [str or None, str] :
+        
+        # append with current state
+        self.add_message( "※ 目前位於 --> {0}".format( current_state ) )
+        
         # concatenate all message
+        reply = ""
         for msg in self.__messages[:-1] :
             reply += """{0}\n""".format( msg )
         reply += """{0}""".format( self.__messages[-1] )
